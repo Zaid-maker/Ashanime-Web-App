2d030294c3bd281aa5633b516e1bec0efbb72d5b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-12-12 14:50:11 +0000

    add firebase configs

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDn087W_nfk7VoOgV3_lj5ke9aQm7B-ZY8",
+  authDomain: "ashmine-fe846.firebaseapp.com",
+  databaseURL: "https://ashmine-fe846-default-rtdb.firebaseio.com",
+  projectId: "ashmine-fe846",
+  storageBucket: "ashmine-fe846.appspot.com",
+  messagingSenderId: "385835066813",
+  appId: "1:385835066813:web:1e527e41d86069391aded4",
+  measurementId: "G-9NC5S1HC5Z",
+};
